57a1ca31b02eb3fa2d20d01bf78419a5b8612472
edingeldein <erich.dingeldein@und.edu>
2019-03-22 13:18:37 -0500

    Set up screen. Got a decent display of the function key set up on the screen.

---
+++ Assets/Scripts/ButtonEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class ButtonEditor : MonoBehaviour
+{
+    private TextMeshProUGUI _tmp;
+
+    public string buttonText = "N/A";
+    public float fontSize = 60f;
+
+    void Start()
+    {
+        _tmp = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        if (_tmp == null)
+            Debug.Log("WTF");
+        SetTMPDefaults();
+    }
+
+    void OnValidate()
+    {
+        _tmp.text = buttonText;
+        _tmp.fontSize = fontSize;
+    }
+
+    void SetTMPDefaults()
+    {
+        _tmp.fontStyle = FontStyles.Bold;
+        _tmp.color = Color.black;
+    }
+}
