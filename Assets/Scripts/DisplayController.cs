30d0e4f8867296c478c74d0ccad2efb88072411d
edingeldein <erich.dingeldein@und.edu>
2019-03-28 11:07:07 -0500

    Adds rudimentary cursor

--- Assets/Scripts/DisplayController.cs
+++ Assets/Scripts/DisplayController.cs
@@ -1,40 +1,70 @@
-﻿using UnityEngine.UI;
+﻿using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 using CustomObjects;
 using CustomEnums;
+using System;
 
 public class DisplayController : MonoBehaviour
 {
 
-    private TextMeshProUGUI _textDisplay;
-    private string _textToDisplay = "";
+    TextMeshProUGUI _textDisplay;
+    string _textToDisplay;
+    string[] _textBuffers = new string[2];
+    int strPos = 0;
+    int tick = 0;
+    bool alive = true;
 
     void Start()
     {
         _textDisplay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
-        Debug.Log(_textDisplay.text);
+
+        _textBuffers[0] = "";
+        _textBuffers[1] = "_";
+        _textToDisplay = _textBuffers[0];
+
+        var cursorCoroutine = Cursor(0.6f);
+        StartCoroutine(cursorCoroutine);
     }
 
     void Update()
     {
+        if (NeedsUpdate())
+            _textToDisplay = _textBuffers[tick % 2];
         _textDisplay.text = _textToDisplay;
+    }
+
+    bool NeedsUpdate()
+    {
+        var tbzClean = _textToDisplay.Equals(_textBuffers[0]);
+        var tboClean = _textToDisplay.Equals(_textBuffers[1]);
+        return !(tbzClean || tboClean);
     }
 
     public void AddText(string text)
     {
-        _textToDisplay += text;
+        _textBuffers[0] = _textBuffers[0].Insert(strPos, text);
+        _textBuffers[1] = _textBuffers[1].Insert(strPos, text);
+        strPos += text.Length;
     }
 
     public void RemoveText(Commands command)
     {
         if (command == Commands.Backspace)
         {
-            if (_textToDisplay.Length == 0) return;
-            _textToDisplay = _textToDisplay.Substring(0, _textToDisplay.Length - 1);
+            if (strPos == 0) return;
+            _textBuffers[0] = _textBuffers[0].Remove(strPos-1);
+            _textBuffers[1] = _textBuffers[1].Substring(0, strPos-1) + "_";
+            strPos--;
         }
         else if (command == Commands.Clear)
-            _textToDisplay = string.Empty;
+        {
+            _textBuffers[0] = string.Empty;
+            _textBuffers[1] = "_";
+            strPos = 0;
+        }
+
     }
 
     public void Navigate(KeyPressData data)
@@ -47,5 +77,20 @@ public class DisplayController : MonoBehaviour
         var commandText = command.ToString();
         Debug.Log(command);
         AddText($"{commandText} ");
+    }
+
+    private IEnumerator Cursor(float waitTime)
+    {        
+        while(alive)
+        {
+            yield return new WaitForSeconds(waitTime);
+            _textToDisplay = _textBuffers[tick % 2];
+            tick++;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        alive = false;
     }
 }
