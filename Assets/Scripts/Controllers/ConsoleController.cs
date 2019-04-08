a02d49b23766d4ebb1eaaa8185ff22e65104112b
edingeldein <erich.dingeldein@und.edu>
2019-04-08 11:04:03 -0500

    Cursor implemented

--- Assets/Scripts/Controllers/ConsoleController.cs
+++ Assets/Scripts/Controllers/ConsoleController.cs
@@ -13,43 +13,48 @@ namespace Controllers
         public RectTransform NewLinePrefab;
         public float HeightDelta = 0.036f;
         public float padding = 5f;
+        public float cursorBlinkTime = 0.3f;
 
         Transform ParentTransform;
         List<GameObject> Lines;
 
         GameObject Cursor;
         CursorBuffers CursorBuf;
+        Text CursorLine;
 
         int CurrentLineNum = -1;
+        bool TextChanged = false;
         Vector2 CurrentLineMin = new Vector2(0f, 1f);
         Text CurrentLine;
-        Text CursorLine;
 
         void Start()
         {
             ParentTransform = gameObject.GetComponent<Transform>();
+
             CursorBuf = new CursorBuffers();
             Cursor = GetCursor();
 
             Lines = new List<GameObject>();
             var firstLine = NewLine();
             CurrentLine = firstLine.GetComponent<Text>();
-                       
+
+            StartCoroutine(CursorTimer(cursorBlinkTime));
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            CursorBuf.Update(CurrentLine.text);
+            CursorLine.text = CursorBuf.Display;
         }
 
         public GameObject GetCursor()
         {
             var go = Instantiate(NewLinePrefab, ParentTransform).gameObject;
 
-            CursorBuffers = new CursorBuffers();
+            CursorBuf = new CursorBuffers();
             var t = go.GetComponent<Text>();
-            t.text = CursorBuffers.Cursor;
+            t.text = CursorBuf.Display;
             t.fontStyle = FontStyle.Bold;
             CursorLine = t;
 
@@ -70,7 +75,7 @@ namespace Controllers
         {
             // Instantiate prefab as game object
             var go = Instantiate(NewLinePrefab, ParentTransform).gameObject;
-            
+
             // Set Text and current editable line
             var t = go.GetComponent<Text>();
             t.text = startingText;
@@ -96,11 +101,13 @@ namespace Controllers
         internal void AddText(string value)
         {
             CurrentLine.text += value;
+            TextChanged = true;
         }
 
         internal void ExecuteCommand(Commands command)
         {
             CurrentLine.text = command.ToString();
+            TextChanged = true;
         }
 
         internal void RemoveText(Commands command)
@@ -108,39 +115,55 @@ namespace Controllers
             if (command == Commands.Backspace && CurrentLine.text.Length > 2)
                 CurrentLine.text = CurrentLine.text.Remove(CurrentLine.text.Length - 1, 1);
             else if (command == Commands.Clear)
-                CurrentLine.text = "> ";            
+                CurrentLine.text = "> ";
+
+            TextChanged = true;
         }
 
-        private IEnumerator ToggleCursor(float waitTime)
+
+
+        private IEnumerator CursorTimer(float waitTime)
         {
             while (true)
             {
                 yield return new WaitForSeconds(waitTime);
-                CursorBuf.Display = CursorBuf.Active ? CursorBuf.Cursor : CursorBuf.NoCursor;
                 CursorBuf.Active = !CursorBuf.Active;
             }
         }
-    }
 
-    internal class CursorBuffers
-    {
-        public bool Active { get; set; }
-        public string Cursor { get; set; }
-        public string NoCursor { get; set; }
-        public string Display { get; set; }
-        public CursorBuffers()
+        internal class CursorBuffers
         {
-            Active = true;
-            Cursor = "  _";
-            NoCursor = "   ";
-            Display = Cursor;
-        }
+            public bool Active { get; set; }
+            public string Cursor { get; set; }
+            public string NoCursor { get; set; }
+            public string Display { get; set; }
+            public CursorBuffers()
+            {
+                Active = true;
+                Cursor = "  _";
+                NoCursor = "   ";
+                Display = Cursor;
+            }
 
-        public void Navigate(int position)
-        {
-            var baseStr = new string(' ', position - 1) + "_";
-            Cursor = baseStr + "_";
-            NoCursor = baseStr + " ";
+            public void Navigate(int position)
+            {
+                var baseStr = new string(' ', position - 1) + "_";
+                Cursor = baseStr + "_";
+                NoCursor = baseStr + " ";
+            }
+
+            public void Update(string currentLine)
+            {
+                var len = currentLine.Length;
+                Cursor = new string(' ', len) + "_";
+                NoCursor = new string(' ', len + 1);
+                Display = CursorBlink();
+            }
+
+            private string CursorBlink()
+            {
+                return Active ? Cursor : NoCursor;
+            }
         }
     }
 }
